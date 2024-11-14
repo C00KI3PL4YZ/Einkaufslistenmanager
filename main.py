4f0300944caf51b1198c2cf360cfc5b25e36c7e3
Joel Feldmann <joel@feldmann.cc>
2024-11-14 12:26:49 +0100

    Done without Database (only CSV)

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-
-
-
 def hinzufuegen_artikel(liste, artikel):
     liste.append(artikel)
-
 
 def entfernen_artikel(liste, artikel):
     liste.remove(artikel)
@@ -23,11 +19,33 @@ def laden_liste(dateiname):
             liste.append(line.strip())
     return liste
 
-
 if __name__ == "__main__":
     main_liste = []
-    dateiname = input("Dateiname: ")
+    dateiname = "file.csv"
     print("Ausgewählte Datei: " + dateiname)
-
-
-
+    main_liste = laden_liste(dateiname)
+    while True:
+        print("1: Artikel hinzufügen")
+        print("2: Artikel entfernen")
+        print("3: Artikel anzeigen")
+        print("4: Liste speichern")
+        print("5: Liste laden")
+        print("6: Beenden")
+        auswahl = input("Auswahl: ")
+        if auswahl == "1":
+            artikel = input("Artikel: ")
+            hinzufuegen_artikel(main_liste, artikel)
+        elif auswahl == "2":
+            artikel = input("Artikel: ")
+            entfernen_artikel(main_liste, artikel)
+        elif auswahl == "3":
+            anzeigen_artikel(main_liste)
+        elif auswahl == "4":
+            speichern_liste(main_liste, dateiname)
+        elif auswahl == "5":
+            main_liste = laden_liste(dateiname)
+        elif auswahl == "6":
+            print(main_liste)
+            break
+        else:
+            print("Ungültige Eingabe")
